50dc107eaa367501d5339ad91e9a126ba25794fb
Oscar Dowson <odow@users.noreply.github.com>
2024-03-01 14:37:03 +1300

    Update x86_64-w64-mingw32.jl

--- src/wrappers/x86_64-w64-mingw32.jl
+++ src/wrappers/x86_64-w64-mingw32.jl
@@ -6,12 +6,12 @@ JLLWrappers.@declare_library_product(libxprs, "xprs.dll")
 
 function __init__()
     JLLWrappers.@generate_init_header()
+    # There's a permission error with the conda binaries
+    chmod(dirname(dirname(artifact_dir)), 0o755; recursive = true)
     JLLWrappers.@init_library_product(
         libxprs,
         "Lib\\site-packages\\xpress\\lib\\xprs.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
     JLLWrappers.@generate_init_footer()
-    # There's a permission error with the conda binaries
-    chmod(dirname(dirname(libxprs)), 0o755; recursive = true)
 end  # __init__()
