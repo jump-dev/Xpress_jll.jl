1565c64236729fa844e0c1506e07e422c1e00b2a
Oscar Dowson <odow@users.noreply.github.com>
2024-06-22 12:13:27 +1200

    Fix chmod check on Windows (#7)

--- src/wrappers/x86_64-w64-mingw32.jl
+++ src/wrappers/x86_64-w64-mingw32.jl
@@ -7,7 +7,9 @@ JLLWrappers.@declare_library_product(libxprs, "xprs.dll")
 function __init__()
     JLLWrappers.@generate_init_header()
     # There's a permission error with the conda binaries
-    chmod(dirname(dirname(artifact_dir)), 0o755; recursive = true)
+    if (stat(artifact_dir).mode & 0o777) != 0o755
+        chmod(artifact_dir, 0o755; recursive = true)
+    end
     JLLWrappers.@init_library_product(
         libxprs,
         "Lib\\site-packages\\xpress\\lib\\xprs.dll",
