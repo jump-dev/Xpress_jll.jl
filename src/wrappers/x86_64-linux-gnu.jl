3e38db8f751d065208b992612097ca7446e0cee5
odow <o.dowson@gmail.com>
2024-06-24 10:07:34 +1200

    Update

--- src/wrappers/x86_64-linux-gnu.jl
+++ src/wrappers/x86_64-linux-gnu.jl
@@ -2,13 +2,13 @@ export libxprs
 
 JLLWrappers.@generate_wrapper_header("Xpress")
 
-JLLWrappers.@declare_library_product(libxprs, "libxprs.so.40.01")
+JLLWrappers.@declare_library_product(libxprs, "libxprs.so.40")
 
 function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libxprs,
-        "lib/python3.9/site-packages/xpress/lib/libxprs.so.40.01",
+        "lib/python3.9/site-packages/xpress/lib/libxprs.so.40",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
     JLLWrappers.@generate_init_footer()
