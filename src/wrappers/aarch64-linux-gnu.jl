4da9c9bd7b6e3a06b2e8e58c16a7c3ed9a3faf27
Oscar Dowson <odow@users.noreply.github.com>
2025-08-28 18:39:45 +1200

    Prep for v9.7.0 (#11)

--- src/wrappers/aarch64-linux-gnu.jl
+++ src/wrappers/aarch64-linux-gnu.jl
@@ -2,13 +2,13 @@ export libxprs
 
 JLLWrappers.@generate_wrapper_header("Xpress")
 
-JLLWrappers.@declare_library_product(libxprs, "libxprs.so.43")
+JLLWrappers.@declare_library_product(libxprs, "libxprs.so.45")
 
 function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libxprs,
-        "lib/libxprs.so.43",
+        "lib/libxprs.so.45",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
     JLLWrappers.@generate_init_footer()
